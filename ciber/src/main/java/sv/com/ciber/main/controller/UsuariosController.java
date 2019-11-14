c98a38f6a883a3e3dd2efc220ae0a2ca16e010f3
william.valdezfgkss <william.valdezfgkss@CDS-SS-2011-005.sv.cds>
2019-11-14 16:58:49 -0600

    se agrego el servicio usuarios y controlador, con la plantilla login. se comento la dependencia de spring security

--- ciber/src/main/java/sv/com/ciber/main/controller/UsuariosController.java
+++ ciber/src/main/java/sv/com/ciber/main/controller/UsuariosController.java
@@ -1,5 +1,25 @@
 package sv.com.ciber.main.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sv.com.ciber.main.model.Usuarios;
+import sv.com.ciber.main.service.UsuariosService;
+
+@RestController
+@RequestMapping(value = "usuarios")
 public class UsuariosController {
 
+	@Autowired
+	UsuariosService us;
+	
+	@GetMapping
+	public List<Usuarios> lista(){
+		List<Usuarios> lista = us.listarTodo();
+		return lista;
+	}
 }
--- ciber/src/main/java/sv/com/ciber/main/service/UsuariosService.java
+++ ciber/src/main/java/sv/com/ciber/main/service/UsuariosService.java
@@ -1,5 +1,35 @@
 package sv.com.ciber.main.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sv.com.ciber.main.model.Usuarios;
+import sv.com.ciber.main.repository.IUsuariosRepository;
+
+@Service
+@Transactional
 public class UsuariosService {
 
+	@Autowired
+	IUsuariosRepository rUser;
+	
+	public List<Usuarios> listarTodo(){
+		return (List<Usuarios>) rUser.findAll();
+	}
+	
+	public void eliminar(Integer id) {
+		rUser.deleteById(id);
+	}
+	
+	public Usuarios getUsuariosById(Integer id) {
+		return rUser.findById(id).get();
+	}
+	
+	public void guardar(Usuarios usuarios) {
+		rUser.save(usuarios);
+	}
 }
